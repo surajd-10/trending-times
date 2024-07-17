2960efee03cee73ae8d64187343b42e083611201
Suraj Dharaskar <suraj.dharaskar@nagarro.com>
2024-07-17 18:44:07 +0530

    fix(article-list): add image in the article

--- src/components/article/index.js
+++ src/components/article/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import "./index.scss";
 
-const Article = ({article, selectedArticle, handleArticleClick}) => {
+const Article = ({ article, selectedArticle, handleArticleClick }) => {
     const { title, abstract, media } = article;
     return (
         <article data-testid="article" className={article.id === selectedArticle.id ? "selected" : ""} onClick={() => handleArticleClick(article)}>
-            <h3>{title}</h3>
-            <p>{abstract}</p>
-            {/* <img src={media[0]?.["media-metadata"][0].url} alt={media[0]?.caption} height={media[0]?.["media-metadata"][0].height} width={media[0]?.["media-metadata"][0].width} /> */}
+            <div>
+                <img src={media[0]?.["media-metadata"][0].url} alt={media[0]?.caption} height={media[0]?.["media-metadata"][0].height} width={media[0]?.["media-metadata"][0].width} />
+            </div>
+            <div>
+                <h3>{title}</h3>
+                <p>{abstract}</p>
+            </div>
         </article>
     )
 }
