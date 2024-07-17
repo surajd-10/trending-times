ff2003fcad29d7cc41f65ef840f2dd72b1af7b60
Suraj Dharaskar <suraj.dharaskar@nagarro.com>
2024-07-17 18:31:15 +0530

    feat: show complete article in iframe

--- src/containers/ArticleDetails/index.js
+++ src/containers/ArticleDetails/index.js
@@ -5,16 +5,18 @@ const ArticleDetails = ({article})=>{
     const {title, abstract, published_date, byline, media, url} = article || {};
     return (
         <div className="details">
-            {/* <iframe width="100%" height="100%" src={article.url}></iframe> */}
-            {article ? 
+            <iframe id="articleDetails" width="100%" height="100%" src={url}></iframe>
+            {/* {article ? 
             <><h2>{title}</h2>
             <p>{abstract}</p>
             <p>Published Date: {published_date}</p>
             <p>By: {byline}</p>
-            <img src={media[0]?.["media-metadata"][2].url} alt={media[0]?.caption} height={media[0]?.["media-metadata"][2].height} width={media[0]?.["media-metadata"][2].width} />
+            <div style={{width: "100%"}}>
+                <img src={media[0]?.["media-metadata"][2].url} alt={media[0]?.caption} />
+            </div>
             <a href={url} target="_blank" rel="noopener noreferrer">
                 Read More
-            </a></>: "No Data To Display"}
+            </a></>: "No Data To Display"} */}
         </div>
     )
 }
