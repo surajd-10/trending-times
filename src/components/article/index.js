68af7260969c804a6c0477bfe20e5858e13d8662
Suraj Dharaskar <suraj.dharaskar@nagarro.com>
2024-07-17 17:07:32 +0530

    fix: make the ui mobile friendly

--- src/components/article/index.js
+++ src/components/article/index.js
@@ -7,7 +7,7 @@ const Article = ({article, selectedArticle, handleArticleClick}) => {
         <article data-testid="article" className={article.id === selectedArticle.id ? "selected" : ""} onClick={() => handleArticleClick(article)}>
             <h3>{title}</h3>
             <p>{abstract}</p>
-            <img src={media[0]?.["media-metadata"][0].url} alt={media[0]?.caption} height={media[0]?.["media-metadata"][0].height} width={media[0]?.["media-metadata"][0].width} />
+            {/* <img src={media[0]?.["media-metadata"][0].url} alt={media[0]?.caption} height={media[0]?.["media-metadata"][0].height} width={media[0]?.["media-metadata"][0].width} /> */}
         </article>
     )
 }
--- src/containers/ArticleDetails/index.js
+++ src/containers/ArticleDetails/index.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./index.scss";
 
 const ArticleDetails = ({article})=>{
+    const {title, abstract, published_date, byline, media, url} = article || {};
     return (
         <div className="details">
             {/* <iframe width="100%" height="100%" src={article.url}></iframe> */}
             {article ? 
-            <><h2>{article.title}</h2>
-            <p>{article.abstract}</p>
-            <p>Published Date: {article.published_date}</p>
-            <p>By: {article.byline}</p>
-            <a href={article.url} target="_blank" rel="noopener noreferrer">
+            <><h2>{title}</h2>
+            <p>{abstract}</p>
+            <p>Published Date: {published_date}</p>
+            <p>By: {byline}</p>
+            <img src={media[0]?.["media-metadata"][2].url} alt={media[0]?.caption} height={media[0]?.["media-metadata"][2].height} width={media[0]?.["media-metadata"][2].width} />
+            <a href={url} target="_blank" rel="noopener noreferrer">
                 Read More
             </a></>: "No Data To Display"}
         </div>
