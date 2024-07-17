1e8e7527ffa75cdd36e7210d0ff2be106dc96ecb
Suraj Dharaskar <suraj.dharaskar@nagarro.com>
2024-07-17 17:56:24 +0530

    feat: add ability to select period of popular articles

--- src/views/home/index.js
+++ src/views/home/index.js
@@ -7,31 +7,55 @@ import "./index.scss";
 const Home = () => {
     const [selectedArticle, setSelectedArticle] = useState(null);
     const [articles, setArticles] = useState([]);
+    const [period, setPeriod] = useState(1);
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
-        const fetchPopularArticles = async () => {
-            try {
-                const { data } = await axios.get("https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=DehHULYr2LZ3Uu614DQjQwmYxMMLPGEC");
-                setArticles(data.results);
-                setSelectedArticle(data.results[0])
-            } catch (error) {
-                console.error(error);
-            }
-        }
         fetchPopularArticles();
     }, [])
+
+    useEffect(() => {
+        fetchPopularArticles();
+    }, [period])
+
+    const fetchPopularArticles = async () => {
+        setLoading(true);
+        try {
+            const { data } = await axios.get(`https://api.nytimes.com/svc/mostpopular/v2/viewed/${period}.json?api-key=DehHULYr2LZ3Uu614DQjQwmYxMMLPGEC`);
+            setArticles(data.results);
+            setSelectedArticle(data.results[0])
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    }
 
     const handleArticleClick = (article) => {
         setSelectedArticle(article);
     }
 
+    const handlePeriodChange = (e) => {
+        setPeriod(e.target.value);
+    }
+
     return (
         <>
-            <header className="mainHeading"><h1>The NY Times</h1></header>
-            <main>
+            <header className="mainHeading">
+                <h1>The NY Times</h1>
+                <label>
+                    Period:
+                    <select value={period} onChange={handlePeriodChange}>
+                        <option value="1">1 Day</option>
+                        <option value="7">7 Days</option>
+                        <option value="30">30 Days</option>
+                    </select>
+                </label>
+            </header>
+            {loading ? "Loading...": <main>
                 <ArticlesList articles={articles} handleArticleClick={handleArticleClick} selectedArticle={selectedArticle} />
                 <ArticleDetails article={selectedArticle} />
-            </main>
+            </main>}
         </>
     )
 }
